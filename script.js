7534d3320742ab0937b3e9ee663edf336a86e624
manuella araujo <silva.manuella@eaportal.org>
2024-06-05 02:07:27 +0000

    criação de um helper responsavel por mudar um tema

--- script.js
+++ script.js
@@ -1,36 +1,14 @@
-let tema ="claro"
+import { trocarTema , verificarTema } from "./helpers/tema-helpers.js"
+
+
 
 const botaoTema=document.querySelector(".tema button")
-botaoTema.addEventListener("click", trocarTema)
-
 const body=document.querySelector("body")
 
+botaoTema.addEventListener("click", () =>{
+    trocarTema( body, botaoTema)
+})
 
-function trocarTema() {
+verificarTema( body, botaoTema)
 
-    if(localStorage.getItem("tema")){
-        tema= localStorage.getItem("tema")
-    }
-    
 
-    if (tema ==="claro"){
-      body.classList.add("escuro")
-      localStorage.setItem("tema", "escuro")
-    x
-    }else{
-        body.classList.remove("escuro")
-        localStorage.setItem("tema", "claro")
-    }
-}
-
-function verificarTema(){
-    if(localStorage.getItem("tema")){
-        tema=localStorage.getItem("tema")
-    }
-
-    if (tema === "escuro"){
-        body.classList.add("escuro")
-    }
-}
-
-verificarTema()
