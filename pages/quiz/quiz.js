6e486433c81fc67baaedc78a828d9c442058f040
manuella araujo <silva.manuella@eaportal.org>
2024-06-05 23:43:39 +0000

    alteração dinamica do assunto da pagina

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -3,8 +3,25 @@ import { verificarTema, trocarTema } from "../../helpers/tema-helpers.js"
 const botaoTema=document.querySelector(".tema button")
 const body=document.querySelector("body")
 
-botaoTema.addEventListener("click", () =>{
+botaoTema.addEventListener("click", () => {
     trocarTema( body, botaoTema)
 })
 
 verificarTema( body, botaoTema)
+
+const assunto =localStorage.getItem("assunto")
+
+function alterarAssunto(){
+
+const divIcone= document.querySelector(".assunto_icone")
+const iconeImg=document.querySelector(".assunto_icone img")
+const assuntoTitulo = document.querySelector(".assunto h1")
+
+    divIcone.classList.add(assunto.toLowerCase())
+    iconeImg.setAttribute("src", `../../assets/images/icon-${assunto.toLowerCase()}.svg`)
+    iconeImg.setAttribute("alt", `ícone de ${assunto}`)
+    assuntoTitulo.innerText=assunto
+}
+
+
+alterarAssunto()
