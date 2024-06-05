8eb241684c6eea1b0d2d0bac2fa86d0c9c890bca
manuella araujo <silva.manuella@eaportal.org>
2024-06-05 12:28:59 +0000

    adicionado o js para a pagina de resultados

---
+++ pages/resultados/resultado.js
@@ -0,0 +1,10 @@
+import { verificarTema, trocarTema } from "../../helpers/tema-helpers.js"
+
+const botaoTema=document.querySelector(".tema button")
+const body=document.querySelector("body")
+
+botaoTema.addEventListener("click", () =>{
+    trocarTema( body, botaoTema)
+})
+
+verificarTema( body, botaoTema)
