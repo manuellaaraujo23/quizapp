2361441c9e8e30629de218cb9cee4a0a423ee52e
manuella araujo <silva.manuella@eaportal.org>
2024-06-06 02:39:02 +0000

    adicionadas as funções de buscar as perguntas e montar as perguntas no html

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -102,7 +102,7 @@ function montarPergunta(){
                     </div>
                 </label>
         </form>
-        <button>Enviar</button>
+        <button>responder</button>
     </section>
 
     `
@@ -113,8 +113,19 @@ function alterarSinais(texto){
     return texto.replace(/</g, "&lt;").replace(/>/g, "&gt;")
 
 }
- 
-function validarResposta(){
+function validarResposta() {
+    const botaoEnviar = document.querySelector(".alternativas button")
+    botaoEnviar.innerText = "Próxima"
+    botaoEnviar.removeEventListener("click" , validarResposta)
+
+
+    if (pergunta === 10) {
+        botaoEnviar.innerText = "Finalizar"
+        botaoEnviar.addEventListener("click" , finalizar)
+    }else {
+        botaoEnviar.addEventListener("click" , proximaPergunta)
+    }botaoEnviar.addEventListener("click" , proximaPergunta)
+
     if (resposta ===quiz.questions[pergunta-1]. answer){
         document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id","correta")
         pontos=pontos+1
@@ -122,7 +133,16 @@ function validarResposta(){
         document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id" , "errada")
         document.querySelector(`label[for='${respostaCorretaId}']`).setAttribute("id" , "correta")
     }
+
+    pergunta=pergunta+1
 }
+
+function finalizar() {
+    localStorage.setItem("pontos" , pontos)
+
+    window.location.href = "../resultado/resultado.html"
+}
+    
 
 
 function guardarResposta(evento){
@@ -132,19 +152,31 @@ function guardarResposta(evento){
    botaoEnviar.addEventListener("click", validarResposta)
 }
 
+function proximaPergunta() {
+    montarPergunta()
+    adicionarEventoInputs()
+
+}
+
+
+function adicionarEventoInputs() {
+    const inputResposta=document.querySelectorAll(".alternativas input")
+    inputResposta.forEach(input =>{
+        input.addEventListener("click", guardarResposta)
+    
+        if (input.value === quiz.questions[pergunta-1].answer) {
+        respostaCorretaId = input.id
+        }
+    })
+}
+
+
  async function iniciar(){
     alterarAssunto()
    await buscarPerguntas()
     montarPergunta()
 }
 
-const inputResposta=document.querySelectorAll(".alternativas input")
-inputResposta.forEach(input =>{
-    input.addEventListener("click", guardarResposta)
 
-    if (input.value === quiz.questions[pergunta-1].answer) {
-    respostaCorretaId = input.id
-    }
-})
 
  iniciar()
